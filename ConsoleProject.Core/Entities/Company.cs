a4fed49aba51d1b942893138ceda1609e00e5a2b
imac <leman.mammadly23@gmail.com>
2023-05-12 21:08:04 +0400

    Create Company Class

---
+++ ConsoleProject.Core/Entities/Company.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleProject.Core.Entities;
+
+public class Company
+{
+    private static int _id;
+    public int Id { get; }
+    public string CompanyName { get; set; }
+
+    public Company()
+    {
+        Id = _id;
+        _id++;
+    }
+}
+
