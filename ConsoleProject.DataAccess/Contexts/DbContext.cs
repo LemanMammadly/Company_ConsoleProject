4b8ae445190d6a570d51db64ed8819beba5148c8
imac <leman.mammadly23@gmail.com>
2023-05-12 21:16:53 +0400

    create DbContext

---
+++ ConsoleProject.DataAccess/Contexts/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleProject.Core.Entities;
+
+namespace ConsoleProject.DataAccess.Contexts;
+
+public static class DbContext
+{
+    public static List<Company> Companies { get; set; } = new();
+    public static List<Department> Departments { get; set; } = new();
+    public static List<Employee> Employees { get; set; } = new();
+}
+
