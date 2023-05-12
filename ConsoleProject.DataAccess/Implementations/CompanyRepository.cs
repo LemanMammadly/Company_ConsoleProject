78fb9d1383a3cb3cc9f11b26bfb60c7d940b5846
imac <leman.mammadly23@gmail.com>
2023-05-12 22:05:55 +0400

    Create CompanyRepository

---
+++ ConsoleProject.DataAccess/Implementations/CompanyRepository.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleProject.Core.Entities;
+using ConsoleProject.DataAccess.Contexts;
+using ConsoleProject.DataAccess.Interfaces;
+
+namespace ConsoleProject.DataAccess.Implementations;
+
+public class CompanyRepository : IRepository<Company>
+{
+    public void Add(Company entity)
+    {
+        DbContext.Companies.Add(entity);
+    }
+
+    public void Delete(Company entity)
+    {
+        DbContext.Companies.Remove(entity);
+    }
+
+    public void Update(Company entity)
+    {
+        Company company = DbContext.Companies.Find(c => c.Id == entity.Id);
+        company.CompanyName = entity.CompanyName;
+    }
+
+    public Company? Get(int id)
+    {
+        return DbContext.Companies.Find(c => c.Id == id);
+    }
+
+    public List<Company> GetAll()
+    {
+        return DbContext.Companies;
+    }
+
+}
+
