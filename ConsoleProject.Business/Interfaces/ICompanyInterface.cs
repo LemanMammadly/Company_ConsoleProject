2c16d5bd8a4b65b3a03b4f20f2a44df244ab3f77
imac <leman.mammadly23@gmail.com>
2023-05-12 22:17:48 +0400

    create ICompanyInterface class

---
+++ ConsoleProject.Business/Interfaces/ICompanyInterface.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleProject.Core.Entities;
+
+namespace ConsoleProject.Business.Interfaces;
+
+public interface ICompanyInterface
+{
+    void Create(string companyName);
+    void Delete(int id);
+    void Update(int id);
+    Company GetById(int id);
+    List<Company> GetAll();
+}
+
